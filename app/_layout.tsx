c32d80d1778a892a3b95ebd23512ed6e431d36f2
Israel Mendes <iomendes505@gmail.com>
2025-09-22 19:50:09 -0300

    refatora a tela de layout, ajustando a formatação do código e corrigindo o nome da tela de confirmação de reserva

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
-import { Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useAuth } from '~/store/auth';
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useAuth } from "~/store/auth";
 
-export default function RootLayout(){
+export default function RootLayout() {
   const { init } = useAuth();
-  useEffect(()=>{ init(); },[]);
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)/login" />
       <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="modals/confirm-booking" options={{ presentation: 'modal' }} />
+      <Stack.Screen
+        name="modals/Booking/confirm-booking"
+        options={{ presentation: "modal" }}
+      />
       <Stack.Screen name="admin/settings" />
       <Stack.Screen name="admin/reports" />
     </Stack>
