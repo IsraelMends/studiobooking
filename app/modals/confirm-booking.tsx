120576987702e8eb571be8203cfe35b57310a2af
Israel Mendes <iomendes505@gmail.com>
2025-09-22 19:50:20 -0300

    remove componente de confirmação de reserva

--- app/modals/confirm-booking.tsx
+++
@@ -1,4 +0,0 @@
-import { View, Text } from 'react-native';
-export default function ConfirmBooking(){
-  return (<View style={{padding:16}}><Text style={{color:'white'}}>Confirmar</Text></View>);
-}
