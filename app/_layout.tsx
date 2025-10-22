5f5ef0e49fc438ae8d5c83613be0a1eb9e709e1b
IsraelMends <iomendes505@gmail.com>
2025-10-22 11:46:52 -0300

    feat: adiciona gerenciamento de notificações e limpeza de reservas expiradas no layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,24 @@
+import React from "react";
 import { Stack } from "expo-router";
 import { useEffect } from "react";
 import { useAuth } from "~/store/auth";
+import { cleanupExpiredBookings } from "../src/services/bookings"; 
+import * as Notifications from "expo-notifications";
 
 export default function RootLayout() {
   const { init } = useAuth();
+
   useEffect(() => {
     init();
+    Notifications.requestPermissionsAsync();
+    
+    const interval = setInterval(() => {
+      cleanupExpiredBookings();
+    }, 5 * 60 * 1000);
+
+    return () => clearInterval(interval);
   }, []);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)/login" />
