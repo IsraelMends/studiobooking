2b8742a41d5b55e27f2aa94486a2344bd374aa86
IsraelMends <iomendes505@gmail.com>
2025-10-23 17:57:28 -0300

    fix: substitui View por SafeAreaView no componente de login

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "expo-router";
 import React from "react";
 
 import styles from "./styles";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
   const router = useRouter();
@@ -64,107 +65,109 @@ export default function Login() {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#0b0f13",
-        padding: 20,
-        justifyContent: "center",
-        gap: 12,
-      }}
-    >
-      <Image
-        source={require("../../assets/logo.png")}
+    <SafeAreaView>
+      <View
         style={{
-          width: 120,
-          height: 120,
-          alignSelf: "center",
-          marginBottom: 20,
-          backgroundColor: "white",
-          borderRadius: 9999,
-          padding: 10,
-        }}
-        resizeMode="contain"
-      />
-
-      <Text
-        style={{
-          color: "white",
-          fontSize: 24,
-          fontWeight: "700",
-          marginBottom: 12,
+          flex: 1,
+          backgroundColor: "#0b0f13",
+          padding: 20,
+          justifyContent: "center",
+          gap: 12,
         }}
       >
-        {isRegister ? "" : "Entrar"}
-      </Text>
+        <Image
+          source={require("../../assets/logo.png")}
+          style={{
+            width: 120,
+            height: 120,
+            alignSelf: "center",
+            marginBottom: 20,
+            backgroundColor: "white",
+            borderRadius: 9999,
+            padding: 10,
+          }}
+          resizeMode="contain"
+        />
 
-      {isRegister && (
+        <Text
+          style={{
+            color: "white",
+            fontSize: 24,
+            fontWeight: "700",
+            marginBottom: 12,
+          }}
+        >
+          {isRegister ? "" : "Entrar"}
+        </Text>
+
+        {isRegister && (
+          <TextInput
+            placeholder="Nome"
+            placeholderTextColor="#9aa0a6"
+            style={styles.input}
+            value={form.name}
+            onChangeText={(t) => setForm({ ...form, name: t })}
+          />
+        )}
         <TextInput
-          placeholder="Nome"
+          placeholder="E-mail"
           placeholderTextColor="#9aa0a6"
           style={styles.input}
-          value={form.name}
-          onChangeText={(t) => setForm({ ...form, name: t })}
+          value={form.email}
+          onChangeText={(t) => setForm({ ...form, email: t })}
+          autoCapitalize="none"
+          keyboardType="email-address"
         />
-      )}
-      <TextInput
-        placeholder="E-mail"
-        placeholderTextColor="#9aa0a6"
-        style={styles.input}
-        value={form.email}
-        onChangeText={(t) => setForm({ ...form, email: t })}
-        autoCapitalize="none"
-        keyboardType="email-address"
-      />
-      <TextInput
-        placeholder="Senha"
-        placeholderTextColor="#9aa0a6"
-        style={styles.input}
-        secureTextEntry
-        value={form.password}
-        onChangeText={(t) => setForm({ ...form, password: t })}
-      />
-      {isRegister && (
-        <>
-          <TextInput
-            placeholder="Telefone"
-            placeholderTextColor="#9aa0a6"
-            style={styles.input}
-            value={form.phone}
-            onChangeText={(t) => setForm({ ...form, phone: t })}
-          />
-          <TextInput
-            placeholder="Faculdade/Empresa"
-            placeholderTextColor="#9aa0a6"
-            style={styles.input}
-            value={form.organization_id}
-            onChangeText={(t) => setForm({ ...form, organization: t })}
-          />
-        </>
-      )}
-
-      {error && <Text style={{ color: "salmon" }}>{error}</Text>}
-
-      <Pressable
-        onPress={onSubmit}
-        disabled={busy}
-        style={{
-          backgroundColor: "#20232a",
-          padding: 12,
-          borderRadius: 12,
-          opacity: busy ? 0.7 : 1,
-        }}
-      >
-        {busy ? (
-          <ActivityIndicator color="#fff" />
-        ) : (
-          <Text
-            style={{ color: "white", textAlign: "center", fontWeight: "700" }}
-          >
-            {isRegister ? "Registrar" : "Entrar"}
-          </Text>
+        <TextInput
+          placeholder="Senha"
+          placeholderTextColor="#9aa0a6"
+          style={styles.input}
+          secureTextEntry
+          value={form.password}
+          onChangeText={(t) => setForm({ ...form, password: t })}
+        />
+        {isRegister && (
+          <>
+            <TextInput
+              placeholder="Telefone"
+              placeholderTextColor="#9aa0a6"
+              style={styles.input}
+              value={form.phone}
+              onChangeText={(t) => setForm({ ...form, phone: t })}
+            />
+            <TextInput
+              placeholder="Faculdade/Empresa"
+              placeholderTextColor="#9aa0a6"
+              style={styles.input}
+              value={form.organization_id}
+              onChangeText={(t) => setForm({ ...form, organization: t })}
+            />
+          </>
         )}
-      </Pressable>
-    </View>
+
+        {error && <Text style={{ color: "salmon" }}>{error}</Text>}
+
+        <Pressable
+          onPress={onSubmit}
+          disabled={busy}
+          style={{
+            backgroundColor: "#20232a",
+            padding: 12,
+            borderRadius: 12,
+            opacity: busy ? 0.7 : 1,
+          }}
+        >
+          {busy ? (
+            <ActivityIndicator color="#fff" />
+          ) : (
+            <Text
+              style={{ color: "white", textAlign: "center", fontWeight: "700" }}
+            >
+              {isRegister ? "Registrar" : "Entrar"}
+            </Text>
+          )}
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
